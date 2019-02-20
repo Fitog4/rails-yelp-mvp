e2678453b3b9fe73234b8eb7abe9ffd15ca804b7
Filippo Tognacci <fitog4@hotmail.it>
2019-02-20 15:29:50 +0100

    implement all functionalities first version

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,23 @@
+class ReviewsController < ApplicationController
+  before_action :set_restaurant, only: [:new, :create]
+
+  def new
+    @review = Review.new(restaurant: @restaurant)
+  end
+
+  def create
+    Review.create(content: review_params[:content], rating: review_params[:rating], restaurant: @restaurant)
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+end
+
