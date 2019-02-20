d525aa4ae46bbc60f7969fbee3a807e185fe5f0f
Filippo Tognacci <fitog4@hotmail.it>
2019-02-20 14:41:40 +0100

    implement restaurants controller actions and views

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Restaurant.create(name: 'Resto1', address: '1 street 1', category: 'belgian')
+Restaurant.create(name: 'Resto2', address: '2 street 2', category: 'italian')
+Restaurant.create(name: 'Resto3', address: '3 street 3', category: 'french')
+Restaurant.create(name: 'Resto4', address: '4 street 4', category: 'chinese')
+Restaurant.create(name: 'Resto5', address: '5 street 5', category: 'japanese')
